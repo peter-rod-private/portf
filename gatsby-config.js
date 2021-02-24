0d60cc39fc50932f5dcafad9a97ccce6139aecbe
peter-rod-private <79568141+peter-rod-private@users.noreply.github.com>
2021-02-24 10:07:24 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,36 +32,21 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    {
-      resolve: "@narative/gatsby-theme-novela",
-      options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
-        authorsPage: true,
-        sources: {
-          //local: true,
-          contentful: true,
-        },
+ plugins: [
+  {
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  },
+  {
+    resolve: '@narative/gatsby-theme-novela',
+    options: {
+      sources: {
+        contentful: true,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Novela by Narative`,
-        short_name: `Novela`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#fff`,
-        display: `standalone`,
-        icon: `src/assets/favicon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-      },
-    },
-  ],
+  },
+]
 };
